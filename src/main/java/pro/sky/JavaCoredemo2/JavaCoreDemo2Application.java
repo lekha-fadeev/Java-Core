c51deffe9f161545a251e174e2a232fd3e51d6c8
79998 <lekha-fadeev@mail.ru>
2023-09-23 09:57:41 +0300

    коммит

---
+++ src/main/java/pro/sky/JavaCoredemo2/JavaCoreDemo2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.JavaCoredemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreDemo2Application.class, args);
+	}
+
+}
